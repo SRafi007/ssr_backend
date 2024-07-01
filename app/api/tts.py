49a5c3d5972ac8809d73c668af8e2c174494cdd7
SRafi007 <sadmansakib.rafiaiub@gmail.com>
2024-07-01 18:03:38 +0600

    first commit

---
+++ app/api/tts.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter , HTTPException
+from gtts import gTTS
+from fastapi.responses import FileResponse
+import os
+
+router= APIRouter()
+
+@router.post('/tts')
+async def create_audiobook(text: str, filename: str = "audiobook.mp3"):
+    try:
+        tts = gTTS(text)
+#       tts.save(filename)
+        return FileResponse(filename, media_type='audio/mpeg', filename=filename)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
